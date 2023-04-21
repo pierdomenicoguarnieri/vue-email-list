cc5fdbd41ecc61361464403c15c7d1f9cd67f1fd
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-21 19:47:00 +0200

    add: getEmail with API

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,10 @@ createApp({
     }    
   },
 
+  methods:{
+    getEmail(){
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(result => {
+        this.emailArray.push(result.data.response)
+      })
+    },
 }).mount("#app")
