e172ead0dc75d783f9838ad6895b6b13f5f534ea
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-26 11:15:42 +0200

    add: input for choosing numbers of email

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,25 @@ createApp({
   data(){
     return{
       emailArray: [],
-      loading: false
+      limit: 10
     }
   },
 
   methods:{
     getEmail(){
-      this.loading = true;
       axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(result => {
         this.emailArray.push(result.data.response)
-        this.loading = false;
       })
     },
 
     getArray(){
       if (this.emailArray.length === 0){
-        for(let i = 0; i < 10; i++){
+        for(let i = 0; i < parseInt(this.limit); i++){
         this.getEmail()
         }
       }else{
         this.emailArray = [];
-        for(let i = 0; i < 10; i++){
+        for(let i = 0; i < parseInt(this.limit); i++){
           this.getEmail()
         }
       }
